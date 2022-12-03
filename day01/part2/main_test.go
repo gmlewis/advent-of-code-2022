1b50431e9e1913d97e5dd8eaf16bd0a38c48f946
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-03 14:40:35 -0500

    Initial commit for day01

---
+++ day01/part2/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gmlewis/advent-of-code-2021/test"
+)
+
+func TestExample(t *testing.T) {
+	want := "Solution: 0\n"
+	test.Runner(t, example1, want, process, &printf)
+}
+
+func BenchmarkExample(b *testing.B) {
+	test.Benchmark(b, "../example1.txt", process, &logf, &printf)
+}
+
+func BenchmarkInput(b *testing.B) {
+	test.Benchmark(b, "../input.txt", process, &logf, &printf)
+}
+
+var example1 = `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+`
