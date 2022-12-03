1128b3b50d5c44c229c34c96554d12e674dffa93
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-03 15:50:16 -0500

    Solve day03
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day03/part1/main.go
+++ day03/part1/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -24,5 +25,33 @@ func process(filename string) {
 	logf("Processing %v ...", filename)
 	buf := must.ReadFile(filename)
 
-	printf("Solution: %v\n", len(buf))
+	sacks := strings.Split(buf, "\n")
+	dups := Map(sacks, findDups)
+	priorities := Map(dups, itemPriority)
+	sum := Reduce(priorities, 0, func(acc, v int) int { return acc + v })
+
+	printf("Solution: %v\n", sum)
+}
+
+func findDups(sack string) string {
+	n := len(sack) / 2
+	in1, in2 := map[rune]int{}, map[rune]int{}
+	for i, r := range sack {
+		if i < n {
+			in1[r]++
+			continue
+		}
+		in2[r]++
+		if _, ok := in1[r]; ok {
+			return string(r)
+		}
+	}
+	log.Fatalf("Could not find dup in %q", sack)
+	return ""
+}
+
+const priorities = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func itemPriority(item string) int {
+	return strings.Index(priorities, item)
 }
--- day03/part2/main.go
+++ day03/part2/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -24,5 +25,41 @@ func process(filename string) {
 	logf("Processing %v ...", filename)
 	buf := must.ReadFile(filename)
 
-	printf("Solution: %v\n", len(buf))
+	sacks := strings.Split(buf, "\n")
+	groups := ChunkEvery(sacks, 3, 3)
+	dups := Map(groups, findDups)
+	// log.Printf("dups=%+v", dups)
+	priorities := Map(dups, itemPriority)
+	sum := Reduce(priorities, 0, func(acc, v int) int { return acc + v })
+
+	printf("Solution: %v\n", sum)
+}
+
+func findDups(sacks []string) string {
+	f := func(sack string) map[rune]int {
+		result := map[rune]int{}
+		for _, r := range sack {
+			result[r]++
+		}
+		return result
+	}
+
+	in1, in2 := f(sacks[0]), f(sacks[1])
+	for _, r := range sacks[2] {
+		if _, ok := in1[r]; !ok {
+			continue
+		}
+		if _, ok := in2[r]; !ok {
+			continue
+		}
+		return string(r)
+	}
+	log.Fatalf("Could not find dup in %+v", sacks)
+	return ""
+}
+
+const priorities = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func itemPriority(item string) int {
+	return strings.Index(priorities, item)
 }
