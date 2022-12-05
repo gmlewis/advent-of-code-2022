590f403aeb4a9528be8105535b9b1b0ac871f4ee
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-05 09:51:12 -0500

    Solve day05
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day05/part2/main.go
+++ day05/part2/main.go
@@ -6,8 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
+	"sort"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
+	"github.com/gmlewis/advent-of-code-2021/maps"
 	"github.com/gmlewis/advent-of-code-2021/must"
 )
 
@@ -20,9 +24,75 @@ func main() {
 	Each(flag.Args(), process)
 }
 
+var (
+	movesRE = regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`)
+)
+
 func process(filename string) {
 	logf("Processing %v ...", filename)
 	buf := must.ReadFile(filename)
 
-	printf("Solution: %v\n", len(buf))
+	parts := strings.Split(buf, "\n\n")
+	stacks := parseStacks(parts[0])
+	stackKeys := maps.Keys(stacks)
+
+	stacks = moveCrates(stacks, parts[1])
+
+	sort.Strings(stackKeys)
+	solution := Reduce(stackKeys, "", takeTop(stacks))
+
+	printf("Solution: %v\n", solution)
+}
+
+type puzT map[string][]string
+
+func moveCrates(puz puzT, cmds string) puzT {
+	for _, cmd := range strings.Split(strings.TrimSpace(cmds), "\n") {
+		m := movesRE.FindStringSubmatch(cmd)
+		if len(m) != 4 {
+			log.Fatalf("Bad move: %v", cmd)
+		}
+		num := must.Atoi(m[1])
+		from := m[2]
+		to := m[3]
+		puz.move(num, from, to)
+	}
+	return puz
+}
+
+func (p puzT) move(num int, from, to string) {
+	if len(p[from]) < num {
+		log.Fatalf("Cannot move %v from %v (%+v) to %v (%+v)!", num, from, p[from], to, p[to])
+	}
+	n := len(p[from])
+	crates := p[from][n-num : n]
+	p[to] = append(p[to], crates...)
+	p[from] = p[from][0 : n-num]
+}
+
+func takeTop(stacks puzT) func(stack, acc string) string {
+	return func(stack, acc string) string {
+		s := stacks[stack]
+		if len(s) == 0 {
+			return acc
+		}
+		return acc + s[len(s)-1]
+	}
+}
+
+func parseStacks(s string) puzT {
+	result := puzT{}
+	lines := strings.Split(s, "\n")
+	lastLine := len(lines) - 1
+	for i := lastLine - 1; i >= 0; i-- {
+		line := lines[i]
+		for x := 1; x < len(line); x += 4 {
+			crate := line[x : x+1]
+			if crate != " " {
+				stack := lines[lastLine][x : x+1]
+				result[stack] = append(result[stack], crate)
+			}
+		}
+	}
+	return result
 }
