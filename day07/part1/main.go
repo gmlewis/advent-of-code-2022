aea3dfa261b030652de7882269a5c577d56d03de
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-07 10:52:34 -0500

    Solve day07 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day07/part1/main.go
+++ day07/part1/main.go
@@ -6,8 +6,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
+	"github.com/gmlewis/advent-of-code-2021/maps"
 	"github.com/gmlewis/advent-of-code-2021/must"
 )
 
@@ -22,7 +24,59 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	cmds := must.ReadSplitFile(filename, "$ ")
 
-	printf("Solution: %v\n", len(buf))
+	filesystem := parseCmds(cmds)
+	allSizes := maps.Values(filesystem)
+	smallDirs := Filter(allSizes, func(v int64) bool { return v <= 100000 })
+	solution := Sum(smallDirs)
+
+	printf("Solution: %v\n", solution)
+}
+
+type puzT map[string]int64
+
+func parseCmds(cmds []string) puzT {
+	puz := puzT{}
+	pwd := "/"
+	parents := map[string][]string{}
+
+	for _, cmd := range cmds {
+		cmd = strings.TrimSpace(cmd)
+		if cmd == "" {
+			continue
+		}
+		switch {
+		case cmd == "cd /":
+			pwd = "/"
+		case cmd == "cd ..":
+			if len(parents[pwd]) < 1 {
+				log.Fatalf("Attempt to cd above / from pwd=%v", pwd)
+			}
+			pwd = parents[pwd][0]
+		case strings.HasPrefix(cmd, "cd "):
+			newPwd := pwd + cmd[3:] + "/"
+			if _, ok := puz[newPwd]; ok {
+				log.Fatalf("Already visited directory '%v'!!!", newPwd)
+			}
+			parents[newPwd] = append([]string{pwd}, parents[pwd]...)
+			pwd = newPwd
+		case strings.HasPrefix(cmd, "ls\n"):
+			for _, file := range strings.Split(cmd, "\n")[1:] {
+				if strings.HasPrefix(file, "dir ") {
+					continue
+				}
+				parts := strings.Split(file, " ")
+				size := int64(must.Atoi(parts[0]))
+				puz[pwd] += size
+				for _, parent := range parents[pwd] {
+					puz[parent] += size
+				}
+			}
+		default:
+			log.Fatalf("Unsupported command: '%v'", cmd)
+		}
+	}
+
+	return puz
 }
--- day07/part1/main_test.go
+++ day07/part1/main_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 95437\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -20,10 +20,6 @@ func BenchmarkInput(b *testing.B) {
 }
 
 var example1 = `
-$ system-update --please --pretty-please-with-sugar-on-top
-<span title="E099 PROGRAMMER IS OVERLY POLITE">Error</span>: No space left on device
-
-
 $ cd /
 $ ls
 dir a
@@ -47,21 +43,4 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k
-
-
-- / (dir)
-  - a (dir)
-    - e (dir)
-      - i (file, size=584)
-    - f (file, size=29116)
-    - g (file, size=2557)
-    - h.lst (file, size=62596)
-  - b.txt (file, size=14848514)
-  - c.dat (file, size=8504156)
-  - d (dir)
-    - j (file, size=4060174)
-    - d.log (file, size=8033020)
-    - d.ext (file, size=5626152)
-    - k (file, size=7214296)
-
 `
