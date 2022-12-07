b4467929e41f9c4f5e32705d19b8f636e94fe5da
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-07 11:01:44 -0500

    Solved day07 part2
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ day07/part2/main.go
@@ -0,0 +1,94 @@
+// -*- compile-command: "go run main.go ../example1.txt ../input.txt"; -*-
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+
+	. "github.com/gmlewis/advent-of-code-2021/enum"
+	"github.com/gmlewis/advent-of-code-2021/maps"
+	"github.com/gmlewis/advent-of-code-2021/must"
+)
+
+var logf = log.Printf
+var printf = fmt.Printf
+
+const (
+	totalDiskSpace = 70000000
+	requiredSpace  = 30000000
+)
+
+func main() {
+	flag.Parse()
+
+	Each(flag.Args(), process)
+}
+
+func process(filename string) {
+	logf("Processing %v ...", filename)
+	cmds := must.ReadSplitFile(filename, "$ ")
+
+	filesystem := parseCmds(cmds)
+	unusedSpace := totalDiskSpace - filesystem["/"]
+	// log.Printf("unused disk space: %v", unusedSpace)
+	neededSpace := requiredSpace - unusedSpace
+	// log.Printf("needed disk space: %v", neededSpace)
+
+	allSizes := maps.Values(filesystem)
+	bigDirs := Filter(allSizes, func(v int64) bool { return v >= neededSpace })
+	sort.Slice(bigDirs, func(a, b int) bool { return bigDirs[a] < bigDirs[b] })
+	solution := bigDirs[0]
+
+	printf("Solution: %v\n", solution)
+}
+
+type puzT map[string]int64
+
+func parseCmds(cmds []string) puzT {
+	puz := puzT{}
+	pwd := "/"
+	parents := map[string][]string{}
+
+	for _, cmd := range cmds {
+		cmd = strings.TrimSpace(cmd)
+		if cmd == "" {
+			continue
+		}
+		switch {
+		case cmd == "cd /":
+			pwd = "/"
+		case cmd == "cd ..":
+			if len(parents[pwd]) < 1 {
+				log.Fatalf("Attempt to cd above / from pwd=%v", pwd)
+			}
+			pwd = parents[pwd][0]
+		case strings.HasPrefix(cmd, "cd "):
+			newPwd := pwd + cmd[3:] + "/"
+			if _, ok := puz[newPwd]; ok {
+				log.Fatalf("Already visited directory '%v'!!!", newPwd)
+			}
+			parents[newPwd] = append([]string{pwd}, parents[pwd]...)
+			pwd = newPwd
+		case strings.HasPrefix(cmd, "ls\n"):
+			for _, file := range strings.Split(cmd, "\n")[1:] {
+				if strings.HasPrefix(file, "dir ") {
+					continue
+				}
+				parts := strings.Split(file, " ")
+				size := int64(must.Atoi(parts[0]))
+				puz[pwd] += size
+				for _, parent := range parents[pwd] {
+					puz[parent] += size
+				}
+			}
+		default:
+			log.Fatalf("Unsupported command: '%v'", cmd)
+		}
+	}
+
+	return puz
+}
---
+++ day07/part2/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gmlewis/advent-of-code-2021/test"
+)
+
+func TestExample(t *testing.T) {
+	want := "Solution: 24933642\n"
+	test.Runner(t, example1, want, process, &printf)
+}
+
+func BenchmarkExample(b *testing.B) {
+	test.Benchmark(b, "../example1.txt", process, &logf, &printf)
+}
+
+func BenchmarkInput(b *testing.B) {
+	test.Benchmark(b, "../input.txt", process, &logf, &printf)
+}
+
+var example1 = `
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+`
