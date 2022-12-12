1f2ec892d3bb998173a533dc652c069601ea07a1
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-12 08:53:19 -0500

    Solve day12 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 
 	"github.com/gmlewis/advent-of-code-2021/algorithm"
 	. "github.com/gmlewis/advent-of-code-2021/enum"
@@ -26,15 +27,20 @@ func process(filename string) {
 	lines := must.ReadFileLines(filename)
 
 	puz := parseLines(lines)
-	log.Printf("start=%v, end=%v", puz.start, puz.end)
+	// log.Printf("start=%v, end=%v", puz.start, puz.end)
+	distances := algorithm.Dijkstra[keyT, int](puz, puz.start, &puz.end, math.MaxInt32)
 
-	printf("Solution: %v\n", len(puz.grid))
+	printf("Solution: %v\n", distances[puz.end])
 }
 
 // puzT implements the algorithm.Graph interface.
 var _ algorithm.Graph[keyT, int] = &puzT{}
 
 type keyT [2]int
+
+func (k keyT) x() int { return k[0] }
+func (k keyT) y() int { return k[1] }
+
 type puzT struct {
 	start keyT
 	end   keyT
@@ -54,19 +60,55 @@ func (p *puzT) parseLine(y int, line string, acc map[keyT]rune) map[keyT]rune {
 		switch r {
 		case 'S':
 			p.start = key
+			acc[key] = 'a'
 		case 'E':
 			p.end = key
+			acc[key] = 'z'
 		}
 	}
 	return acc
 }
 
 func (p *puzT) Distance(from, to keyT) int {
-	return 0
+	limit := 1 + p.grid[from]
+	if v, ok := p.grid[to]; !ok || v > limit {
+		return math.MaxInt32
+	}
+
+	dx := to.x() - from.x()
+	if dx < 0 {
+		dx = -dx
+	}
+	dy := to.y() - from.y()
+	if dy < 0 {
+		dy = -dy
+	}
+	// log.Printf("Distance(from=%v=%c, to=%v=%c): %v", from, p.grid[from], to, p.grid[to], dx+dy)
+	return dx + dy
 }
 
-func (p *puzT) Each(func(key keyT)) {
+func (p *puzT) Each(f func(key keyT)) {
+	for k := range p.grid {
+		f(k)
+	}
 }
 
 func (p *puzT) EachNeighbor(from keyT, f func(from, to keyT)) {
+	// log.Printf("EachNeighbor(from=%v=%c)", from, p.grid[from])
+	up := keyT{from.x(), from.y() - 1}
+	if _, ok := p.grid[up]; ok {
+		f(from, up)
+	}
+	down := keyT{from.x(), from.y() + 1}
+	if _, ok := p.grid[down]; ok {
+		f(from, down)
+	}
+	left := keyT{from.x() - 1, from.y()}
+	if _, ok := p.grid[left]; ok {
+		f(from, left)
+	}
+	right := keyT{from.x() + 1, from.y()}
+	if _, ok := p.grid[right]; ok {
+		f(from, right)
+	}
 }
