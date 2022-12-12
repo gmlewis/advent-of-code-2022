e810a3c2ad5c23d5300633a0b9529c918ee024e8
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-12 08:01:09 -0500

    Start day12
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gmlewis/advent-of-code-2021/algorithm"
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
 )
@@ -22,7 +23,50 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	puz := parseLines(lines)
+	log.Printf("start=%v, end=%v", puz.start, puz.end)
+
+	printf("Solution: %v\n", len(puz.grid))
+}
+
+// puzT implements the algorithm.Graph interface.
+var _ algorithm.Graph[keyT, int] = &puzT{}
+
+type keyT [2]int
+type puzT struct {
+	start keyT
+	end   keyT
+	grid  map[keyT]rune
+}
+
+func parseLines(lines []string) *puzT {
+	puz := &puzT{}
+	puz.grid = ReduceWithIndex(lines, map[keyT]rune{}, puz.parseLine)
+	return puz
+}
+
+func (p *puzT) parseLine(y int, line string, acc map[keyT]rune) map[keyT]rune {
+	for x, r := range line {
+		key := keyT{x, y}
+		acc[key] = r
+		switch r {
+		case 'S':
+			p.start = key
+		case 'E':
+			p.end = key
+		}
+	}
+	return acc
+}
+
+func (p *puzT) Distance(from, to keyT) int {
+	return 0
+}
+
+func (p *puzT) Each(func(key keyT)) {
+}
+
+func (p *puzT) EachNeighbor(from keyT, f func(from, to keyT)) {
 }
