1b00d3e749346467e47c6db815469ed3c48492bf
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-13 13:19:21 -0500

    Solve day13 part2
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day13/part2/main.go
+++ day13/part2/main.go
@@ -6,6 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
+	"sort"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -22,7 +25,168 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
+	lines = append(lines, "[[2]]", "[[6]]")
+	lines = Filter(lines, func(s string) bool { return s != "" })
+	packets := Map(lines, func(s string) *packetT { return parsePacket(s) })
 
-	printf("Solution: %v\n", len(buf))
+	sort.Slice(packets, func(a, b int) bool { return compare(packets[a], packets[b]) < 0 })
+	decoderKey := 1
+	for i, p := range packets {
+		s := p.String()
+		switch s {
+		case "[[2]]":
+			decoderKey *= (i + 1)
+		case "[[6]]":
+			decoderKey *= (i + 1)
+			break
+		}
+	}
+
+	printf("Solution: %v\n", decoderKey)
+}
+
+type packetT struct {
+	v   *int
+	els []*packetT
+}
+
+func isInOrder(pair string) bool {
+	parts := strings.Split(pair, "\n")
+	p1 := parsePacket(parts[0])
+	p2 := parsePacket(parts[1])
+	return compare(p1, p2) <= 0
+}
+
+func compare(p1, p2 *packetT) int {
+	switch {
+	// If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+	case p1 == nil && p2 == nil:
+		return 0
+	// If the left list runs out of items first, the inputs are in the right order.
+	case p1 == nil:
+		return -1
+	// If the right list runs out of items first, the inputs are not in the right order.
+	case p2 == nil:
+		return 1
+
+	// If both values are integers, the lower integer should come first.
+	// If the left integer is lower than the right integer, the inputs are in the right order.
+	case p1.v != nil && p2.v != nil && *p1.v < *p2.v:
+		return -1
+	// If the left integer is higher than the right integer, the inputs are not in the right order.
+	case p1.v != nil && p2.v != nil && *p1.v > *p2.v:
+		return 1
+	// Otherwise, the inputs are the same integer; continue checking the next part of the input.
+	case p1.v != nil && p2.v != nil && *p1.v == *p2.v:
+		return 0
+
+	// If both values are lists, compare the first value of each list, then the second value, and so on.
+	case p1.v == nil && p2.v == nil:
+		n := len(p1.els)
+		if len(p2.els) > n {
+			n = len(p2.els)
+		}
+
+		for i := 0; i < n; i++ {
+			var el1, el2 *packetT
+			if i < len(p1.els) {
+				el1 = p1.els[i]
+			}
+			if i < len(p2.els) {
+				el2 = p2.els[i]
+			}
+			cmp := compare(el1, el2)
+			if cmp != 0 {
+				return cmp
+			}
+		}
+
+		if len(p1.els) < len(p2.els) {
+			return -1
+		}
+		if len(p1.els) > len(p2.els) {
+			return 1
+		}
+		return 0
+
+	// If exactly one value is an integer, convert the integer to a list which contains that integer
+	// as its only value, then retry the comparison.
+	// For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2);
+	// the result is then found by instead comparing [0,0,0] and [2].
+	case p1.v == nil: // p1 is a list. Convert p2 to list.
+		right := &packetT{els: []*packetT{p2}}
+		return compare(p1, right)
+
+	case p2.v == nil: // p2 is a list. Convert p1 to list.
+		left := &packetT{els: []*packetT{p1}}
+		return compare(left, p2)
+
+	default:
+		log.Fatalf("Unhandled case: packetsEqual:\np1=%#v\np2=%#v", p1, p2)
+		return 1
+	}
+}
+
+func (p *packetT) String() string {
+	if p == nil {
+		return "<nil>"
+	}
+	switch {
+	case p.v != nil:
+		return fmt.Sprintf("%v", *p.v)
+	default:
+		parts := make([]string, 0, len(p.els))
+		for _, el := range p.els {
+			parts = append(parts, el.String())
+		}
+		return fmt.Sprintf("[%v]", strings.Join(parts, ","))
+	}
+}
+
+var (
+	numRE = regexp.MustCompile(`^(\d+)`)
+)
+
+func parsePacket(s string) *packetT {
+	p, next := parseNext(nil, s)
+	if next != "" {
+		log.Fatalf("parsePacket('%v'): next has value: %q\n%v", s, next, p)
+	}
+	return p
+}
+
+func parseNext(p *packetT, s string) (root *packetT, next string) {
+	m := numRE.FindStringSubmatch(s)
+	if len(m) == 2 {
+		v := must.Atoi(m[1])
+		if p == nil {
+			log.Fatalf("parseNext('%v') expected p!=nil", s)
+		}
+		p.els = append(p.els, &packetT{v: &v})
+		next = s[len(m[1]):]
+		return p, next
+	}
+
+	switch {
+	case strings.HasPrefix(s, "["):
+		child := &packetT{}
+		if p == nil {
+			p = child
+		} else {
+			p.els = append(p.els, child)
+		}
+		next = s[1:]
+		for next != "" {
+			if strings.HasPrefix(next, "]") {
+				return p, next[1:]
+			}
+			_, next = parseNext(child, next)
+			if strings.HasPrefix(next, ",") {
+				next = next[1:]
+			}
+		}
+	}
+
+	return p, next
 }
--- day13/part2/main_test.go
+++ day13/part2/main_test.go
@@ -1,13 +1,229 @@
 package main
 
 import (
+	"log"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/test"
+	"github.com/google/go-cmp/cmp"
 )
 
+func TestCompare(t *testing.T) {
+	tests := []struct {
+		name string
+		p1   string
+		p2   string
+		want int
+	}{
+		{
+			name: "equal",
+			p1:   "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+			p2:   "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+			want: 0,
+		},
+		{
+			name: "== Pair 1 ==",
+			p1:   "[1,1,3,1,1]",
+			p2:   "[1,1,5,1,1]",
+			// - Compare [1,1,3,1,1] vs [1,1,5,1,1]
+			//   - Compare 1 vs 1
+			//   - Compare 1 vs 1
+			//   - Compare 3 vs 5
+			//     - Left side is smaller, so inputs are in the right order
+			want: -1,
+		},
+		{
+			name: "== Pair 2 ==",
+			p1:   "[[1],[2,3,4]]",
+			p2:   "[[1],4]",
+			// - Compare [[1],[2,3,4]] vs [[1],4]
+			//   - Compare [1] vs [1]
+			//     - Compare 1 vs 1
+			//   - Compare [2,3,4] vs 4
+			//     - Mixed types; convert right to [4] and retry comparison
+			//     - Compare [2,3,4] vs [4]
+			//       - Compare 2 vs 4
+			//         - Left side is smaller, so inputs are in the right order
+			want: -1,
+		},
+		{
+			name: "== Pair 3 ==",
+			p1:   "[9]",
+			p2:   "[[8,7,6]]",
+			// - Compare [9] vs [[8,7,6]]
+			//   - Compare 9 vs [8,7,6]
+			//     - Mixed types; convert left to [9] and retry comparison
+			//     - Compare [9] vs [8,7,6]
+			//       - Compare 9 vs 8
+			//         - Right side is smaller, so inputs are not in the right order
+			want: 1,
+		},
+		{
+			name: "== Pair 4 ==",
+			p1:   "[[4,4],4,4]",
+			p2:   "[[4,4],4,4,4]",
+			// - Compare [[4,4],4,4] vs [[4,4],4,4,4]
+			//   - Compare [4,4] vs [4,4]
+			//     - Compare 4 vs 4
+			//     - Compare 4 vs 4
+			//   - Compare 4 vs 4
+			//   - Compare 4 vs 4
+			//   - Left side ran out of items, so inputs are in the right order
+			want: -1,
+		},
+		{
+			name: "== Pair 5 ==",
+			p1:   "[7,7,7,7]",
+			p2:   "[7,7,7]",
+			// - Compare [7,7,7,7] vs [7,7,7]
+			//   - Compare 7 vs 7
+			//   - Compare 7 vs 7
+			//   - Compare 7 vs 7
+			//   - Right side ran out of items, so inputs are not in the right order
+			want: 1,
+		},
+		{
+			name: "== Pair 6 ==",
+			p1:   "[]",
+			p2:   "[3]",
+			// - Compare [] vs [3]
+			//   - Left side ran out of items, so inputs are in the right order
+			want: -1,
+		},
+		{
+			name: "== Pair 7 ==",
+			p1:   "[[[]]]",
+			p2:   "[[]]",
+			// - Compare [[[]]] vs [[]]
+			//   - Compare [[]] vs []
+			//     - Right side ran out of items, so inputs are not in the right order
+			want: 1,
+		},
+		{
+			name: "== Pair 8 ==",
+			p1:   "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+			p2:   "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+			// - Compare [1,[2,[3,[4,[5,6,7]]]],8,9] vs [1,[2,[3,[4,[5,6,0]]]],8,9]
+			//   - Compare 1 vs 1
+			//   - Compare [2,[3,[4,[5,6,7]]]] vs [2,[3,[4,[5,6,0]]]]
+			//     - Compare 2 vs 2
+			//     - Compare [3,[4,[5,6,7]]] vs [3,[4,[5,6,0]]]
+			//       - Compare 3 vs 3
+			//       - Compare [4,[5,6,7]] vs [4,[5,6,0]]
+			//         - Compare 4 vs 4
+			//         - Compare [5,6,7] vs [5,6,0]
+			//           - Compare 5 vs 5
+			//           - Compare 6 vs 6
+			//           - Compare 7 vs 0
+			//             - Right side is smaller, so inputs are not in the right order
+			//
+			// 	}
+			want: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p1 := parsePacket(tt.p1)
+			p2 := parsePacket(tt.p2)
+			got := compare(p1, p2)
+
+			if got != tt.want {
+				t.Errorf("compare = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParsePacket(t *testing.T) {
+	tests := []struct {
+		s    string
+		want *packetT
+	}{
+		{
+			s:    "[]",
+			want: &packetT{},
+		},
+		{
+			s: "[1,1,3,1,1]",
+			want: &packetT{
+				els: makeEls(1, 1, 3, 1, 1),
+			},
+		},
+		{
+			s: "[[1],[2,3,4]]",
+			want: &packetT{
+				els: []*packetT{
+					{els: makeEls(1)},
+					{els: makeEls(2, 3, 4)},
+				},
+			},
+		},
+		{
+			s: "[[[]]]",
+			want: &packetT{
+				els: []*packetT{
+					{els: []*packetT{
+						{els: []*packetT{}},
+					}},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			got := parsePacket(tt.s)
+
+			if gotStr := got.String(); gotStr != tt.s {
+				t.Errorf("parsePacket.String = '%v', want '%v'", gotStr, tt.s)
+			}
+
+			if diff := cmp.Diff(tt.want, got, cmp.Comparer(packetsEqual)); diff != "" {
+				t.Errorf("parsePacket('%v') mismatch (-want, +got):\n%v", tt.s, diff)
+			}
+		})
+	}
+}
+
+func intPtr(v int) *int { return &v }
+func makeEls(vs ...int) []*packetT {
+	p := make([]*packetT, 0, len(vs))
+	for _, v := range vs {
+		p = append(p, &packetT{v: intPtr(v)})
+	}
+	return p
+}
+
+func packetsEqual(p1, p2 *packetT) bool {
+	switch {
+	case p1 == nil && p2 == nil:
+		return true
+	case p1 == nil || p2 == nil:
+		return false
+	case p1.v == nil && p2.v == nil:
+		if len(p1.els) != len(p2.els) {
+			return false
+		}
+		for i, el1 := range p1.els {
+			el2 := p2.els[i]
+			if !packetsEqual(el1, el2) {
+				return false
+			}
+		}
+		return true
+	case p1.v == nil || p2.v == nil:
+		return false
+	case p1.v != nil && p2.v != nil:
+		return *p1.v == *p2.v
+	default:
+		log.Fatalf("Unhandled case: packetsEqual:\np1=%#v\np2=%#v", p1, p2)
+		return false
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 140\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -43,71 +259,4 @@ var example1 = `
 
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]
-
-
-== Pair 1 ==
-- Compare [1,1,3,1,1] vs [1,1,5,1,1]
-  - Compare 1 vs 1
-  - Compare 1 vs 1
-  - Compare 3 vs 5
-    - Left side is smaller, so inputs are in the right order
-
-== Pair 2 ==
-- Compare [[1],[2,3,4]] vs [[1],4]
-  - Compare [1] vs [1]
-    - Compare 1 vs 1
-  - Compare [2,3,4] vs 4
-    - Mixed types; convert right to [4] and retry comparison
-    - Compare [2,3,4] vs [4]
-      - Compare 2 vs 4
-        - Left side is smaller, so inputs are in the right order
-
-== Pair 3 ==
-- Compare [9] vs [[8,7,6]]
-  - Compare 9 vs [8,7,6]
-    - Mixed types; convert left to [9] and retry comparison
-    - Compare [9] vs [8,7,6]
-      - Compare 9 vs 8
-        - Right side is smaller, so inputs are not in the right order
-
-== Pair 4 ==
-- Compare [[4,4],4,4] vs [[4,4],4,4,4]
-  - Compare [4,4] vs [4,4]
-    - Compare 4 vs 4
-    - Compare 4 vs 4
-  - Compare 4 vs 4
-  - Compare 4 vs 4
-  - Left side ran out of items, so inputs are in the right order
-
-== Pair 5 ==
-- Compare [7,7,7,7] vs [7,7,7]
-  - Compare 7 vs 7
-  - Compare 7 vs 7
-  - Compare 7 vs 7
-  - Right side ran out of items, so inputs are not in the right order
-
-== Pair 6 ==
-- Compare [] vs [3]
-  - Left side ran out of items, so inputs are in the right order
-
-== Pair 7 ==
-- Compare [[[]]] vs [[]]
-  - Compare [[]] vs []
-    - Right side ran out of items, so inputs are not in the right order
-
-== Pair 8 ==
-- Compare [1,[2,[3,[4,[5,6,7]]]],8,9] vs [1,[2,[3,[4,[5,6,0]]]],8,9]
-  - Compare 1 vs 1
-  - Compare [2,[3,[4,[5,6,7]]]] vs [2,[3,[4,[5,6,0]]]]
-    - Compare 2 vs 2
-    - Compare [3,[4,[5,6,7]]] vs [3,[4,[5,6,0]]]
-      - Compare 3 vs 3
-      - Compare [4,[5,6,7]] vs [4,[5,6,0]]
-        - Compare 4 vs 4
-        - Compare [5,6,7] vs [5,6,0]
-          - Compare 5 vs 5
-          - Compare 6 vs 6
-          - Compare 7 vs 0
-            - Right side is smaller, so inputs are not in the right order
-
 `
