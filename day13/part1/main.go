efcc68bf4c19856c8c4a02f4e43fd8cdd41593fd
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-13 11:28:26 -0500

    wip
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day13/part1/main.go
+++ day13/part1/main.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -24,5 +26,78 @@ func process(filename string) {
 	logf("Processing %v ...", filename)
 	pairs := must.ReadSplitFile(filename, "\n\n")
 
-	printf("Solution: %v\n", len(pairs))
+	var sum int
+	for i, pair := range pairs {
+		if isInOrder(pair) {
+			sum += (i + 1)
+		}
+	}
+
+	printf("Solution: %v\n", sum)
+}
+
+type packetT struct {
+	v   *int
+	els []*packetT
+}
+
+func isInOrder(pair string) bool {
+	// parts := strings.Split(pair, "\n")
+	// p1 := parsePacket(parts[0])
+	// p2 := parsePacket(parts[1])
+	return true
+}
+
+func parsePacket(s string) *packetT {
+	p := &packetT{}
+	if next, _ := p.parseNext(s); next != "" {
+		log.Fatalf("parsePacket('%v'): next has value: %q\n%v", s, next, p)
+	}
+	return p
+}
+
+var (
+	numRE = regexp.MustCompile(`^(\d+)`)
+)
+
+func (p *packetT) String() string {
+}
+
+func (p *packetT) parseNext(s string) (next string, eol bool) {
+	switch {
+	case s == "":
+		return "", false
+	case strings.HasPrefix(s, ","):
+		return s[1:], false
+	case strings.HasPrefix(s, "]"):
+		return s[1:], true
+	case strings.HasPrefix(s, "["):
+		next = s[1:]
+		for {
+			sp := &packetT{}
+			next, eol = sp.parseNext(next)
+			p.els = append(p.els, sp)
+			if eol {
+				return next, false
+			}
+			switch {
+			case strings.HasPrefix(next, ","):
+				next = next[1:]
+			case strings.HasPrefix(next, "]"):
+				next = next[1:]
+				return next, true
+			default:
+				log.Fatalf("parseNext('%v') expected comma or ']', but got %q", s, next)
+			}
+		}
+	default:
+		m := numRE.FindStringSubmatch(s)
+		if len(m) == 2 {
+			v := must.Atoi(m[1])
+			p.v = &v
+			return s[len(m[1]):], false
+		}
+		log.Fatalf("parseNext unhandled case: %q", s)
+	}
+	return "error", false
 }
--- day13/part1/main_test.go
+++ day13/part1/main_test.go
@@ -1,13 +1,95 @@
 package main
 
 import (
+	"log"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/test"
+	"github.com/google/go-cmp/cmp"
 )
 
+func TestParsePacket(t *testing.T) {
+	tests := []struct {
+		s    string
+		want *packetT
+	}{
+		{
+			s:    "",
+			want: &packetT{},
+		},
+		{
+			s: "[1,1,3,1,1]",
+			want: &packetT{
+				els: makeEls(1, 1, 3, 1, 1),
+			},
+		},
+		{
+			s: "[[1],[2,3,4]]",
+			want: &packetT{
+				els: []*packetT{
+					{els: makeEls(1)},
+					{els: makeEls(2, 3, 4)},
+				},
+			},
+		},
+		{
+			s: "[[[]]]",
+			want: &packetT{
+				els: []*packetT{
+					{els: []*packetT{}},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			got := parsePacket(tt.s)
+			if diff := cmp.Diff(tt.want, got, cmp.Comparer(packetsEqual)); diff != "" {
+				t.Errorf("parsePacket('%v') mismatch (-want, +got):\n%v", tt.s, diff)
+			}
+		})
+	}
+}
+
+func intPtr(v int) *int { return &v }
+func makeEls(vs ...int) []*packetT {
+	p := make([]*packetT, 0, len(vs))
+	for _, v := range vs {
+		p = append(p, &packetT{v: intPtr(v)})
+	}
+	return p
+}
+
+func packetsEqual(p1, p2 *packetT) bool {
+	switch {
+	case p1 == nil && p2 == nil:
+		return true
+	case p1 == nil || p2 == nil:
+		return false
+	case p1.v == nil && p2.v == nil:
+		if len(p1.els) != len(p2.els) {
+			return false
+		}
+		for i, el1 := range p1.els {
+			el2 := p2.els[i]
+			if !packetsEqual(el1, el2) {
+				return false
+			}
+		}
+		return true
+	case p1.v == nil || p2.v == nil:
+		return false
+	case p1.v != nil && p2.v != nil:
+		return *p1.v == *p2.v
+	default:
+		log.Fatalf("Unhandled case: packetsEqual:\np1=%#v\np2=%#v", p1, p2)
+		return false
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 36\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
