92f4ba12319ce7284fbb71df66fd5aa5272a0002
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-14 10:27:56 -0500

    Solve day14 part2
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day14/part2/main.go
+++ day14/part2/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -20,9 +21,133 @@ func main() {
 	Each(flag.Args(), process)
 }
 
+const xStart = 500
+
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	puz := parseLines(lines)
+	grains := puz.dropGrains()
+
+	printf("Solution: %v\n", grains)
+}
+
+type keyT [2]int
+
+func (k keyT) x() int { return k[0] }
+func (k keyT) y() int { return k[1] }
+
+type puzT struct {
+	xmin int
+	xmax int
+	ymax int
+	grid map[keyT]rune
+}
+
+func (p *puzT) dropGrains() int {
+	var grains int
+	for p.dropOneGrain() {
+		grains++
+	}
+	return grains
+}
+
+func (p *puzT) dropOneGrain() bool {
+	key := keyT{xStart, -1}
+	dKey := func() keyT { return keyT{key.x(), key.y() + 1} }
+	if p.grid[dKey()] != 0 {
+		return false
+	}
+	dlKey := func() keyT { return keyT{key.x() - 1, key.y() + 1} }
+	drKey := func() keyT { return keyT{key.x() + 1, key.y() + 1} }
+	down := func() rune { return p.grid[dKey()] }
+	downLeft := func() rune { return p.grid[dlKey()] }
+	downRight := func() rune { return p.grid[drKey()] }
+
+	for {
+		d := down()
+		dl := downLeft()
+		dr := downRight()
+		switch {
+		case d == 0:
+			key = dKey()
+			if key.y() >= p.ymax+1 {
+				p.grid[key] = 'o'
+				return true
+			}
+		case dl != 0 && dr != 0:
+			p.grid[key] = 'o'
+			return true
+		case dl == 0:
+			key = dlKey()
+		case dl != 0 && dr == 0:
+			key = drKey()
+		default:
+			log.Fatalf("unhandled case: key=%v, d=%v, dl=%v, dr=%v", key, d, dl, dr)
+		}
+		if key.y() == p.ymax+1 && p.grid[key] == 0 {
+			p.grid[key] = 'o'
+			return true
+		}
+	}
+}
+
+func parseLines(lines []string) *puzT {
+	p := &puzT{xmin: xStart, xmax: xStart, grid: map[keyT]rune{}}
+
+	for _, line := range lines {
+		parts := strings.Split(line, " -> ")
+		p.drawRocks(parts)
+	}
+
+	return p
+}
+
+func (p *puzT) drawRocks(rocks []string) {
+	var lastKey keyT
+	for i, rock := range rocks {
+		parts := strings.Split(rock, ",")
+		x := must.Atoi(parts[0])
+		y := must.Atoi(parts[1])
+		if x < p.xmin {
+			p.xmin = x
+		}
+		if x > p.xmax {
+			p.xmax = x
+		}
+		if y > p.ymax {
+			p.ymax = y
+		}
+		key := keyT{x, y}
+		if i == 0 {
+			lastKey = key
+			continue
+		}
+		p.drawLine(lastKey, key, '#')
+		lastKey = key
+	}
+}
+
+func cmp(v int) int {
+	switch {
+	case v < 0:
+		return -1
+	case v > 0:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func (p *puzT) drawLine(from, to keyT, r rune) {
+	dx := cmp(to.x() - from.x())
+	dy := cmp(to.y() - from.y())
+
+	p.grid[to] = r
+	for from != to {
+		p.grid[from] = r
+		from[0] += dx
+		from[1] += dy
+	}
 }
