0e0be50209698b37530bd6b567b2b7b332c98bfd
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-15 08:01:41 -0500

    Solve day15 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day15/part1/main.go
+++ day15/part1/main.go
@@ -39,28 +39,65 @@ func process(filename string) {
 }
 
 type keyT [2]int
+
+func (k keyT) x() int { return k[0] }
+func (k keyT) y() int { return k[1] }
+
 type puzT struct {
-	xmin int
-	xmax int
-	ymin int
-	ymax int
-	grid map[keyT]pairT
+	xmin    int
+	xmax    int
+	ymin    int
+	ymax    int
+	grid    map[keyT]rune
+	sensors map[keyT]*pairT // keyed by sensor location
 }
 type pairT struct {
-	sensor keyT
 	beacon keyT
 	dx     int // positive
 	dy     int // positive
 }
 
-func (p *puzT) noBeacons(row int) int {
-	return 0
+func (p *puzT) noBeacons(y int) int {
+	var impossibleBeacons int
+	// log.Printf("Searching from x=%v to x=%v", p.xmin, p.xmax)
+	for x := p.xmin; x <= p.xmax; x++ {
+		if !p.isBeaconPossible(x, y) {
+			impossibleBeacons++
+		}
+	}
+	return impossibleBeacons
+}
+
+func (p *puzT) isBeaconPossible(x, y int) bool {
+	if p.grid[keyT{x, y}] == 'B' {
+		return true
+	}
+	for s, pair := range p.sensors {
+		if !pair.isBeaconPossible(s, x, y) {
+			// log.Printf("beacon impossible at (%v,%v) because of sensor %v", x, y, s)
+			return false
+		}
+	}
+	return true
+}
+
+func (p *pairT) isBeaconPossible(sensor keyT, x, y int) bool {
+	dx := sensor.x() - x
+	if dx < 0 {
+		dx = -dx
+	}
+	dy := sensor.y() - y
+	if dy < 0 {
+		dy = -dy
+	}
+	possible := dx+dy > p.dx+p.dy
+	return possible
 }
 
 var lineRE = regexp.MustCompile(`=(-?\d+)`)
 
 func parsePuzzle(lines []string) *puzT {
-	p := &puzT{grid: map[keyT]rune{}}
+	p := &puzT{grid: map[keyT]rune{}, sensors: map[keyT]*pairT{}}
 	for _, line := range lines {
 		m := lineRE.FindAllStringSubmatch(line, -1)
 		if len(m) != 4 {
@@ -68,10 +105,43 @@ func parsePuzzle(lines []string) *puzT {
 		}
 		sx := must.Atoi(m[0][1])
 		sy := must.Atoi(m[1][1])
-		p.grid[keyT{sx, sy}] = 'S'
+		sensor := keyT{sx, sy}
 		bx := must.Atoi(m[2][1])
 		by := must.Atoi(m[3][1])
-		p.grid[keyT{bx, by}] = 'S'
+		beacon := keyT{bx, by}
+		dx, dy := sx-bx, sy-by
+		if dx < 0 {
+			dx = -dx
+		}
+		if dy < 0 {
+			dy = -dy
+		}
+		p.grid[sensor] = 'S'
+		p.grid[beacon] = 'B'
+		p.updateBounds(sx-dx, sy-dy)
+		p.updateBounds(sx+dx, sy+dy)
+		p.updateBounds(bx-dx, by-dy)
+		p.updateBounds(bx+dx, by+dy)
+		p.sensors[sensor] = &pairT{
+			beacon: beacon,
+			dx:     dx,
+			dy:     dy,
+		}
 	}
 	return p
+}
+
+func (p *puzT) updateBounds(x, y int) {
+	if x < p.xmin {
+		p.xmin = x
+	}
+	if x > p.xmax {
+		p.xmax = x
+	}
+	if y < p.ymin {
+		p.ymin = y
+	}
+	if y > p.ymax {
+		p.ymax = y
+	}
 }
