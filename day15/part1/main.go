08a70aec87be2efca01892454c6a3792960ebe02
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-15 06:58:42 -0500

    Start of day15
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day15/part1/main.go
+++ day15/part1/main.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -22,7 +24,54 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	puz := parsePuzzle(lines)
+
+	rowY := 10
+	if strings.Contains(filename, "input.txt") {
+		rowY = 2000000
+	}
+
+	solution := puz.noBeacons(rowY)
+
+	printf("Solution: %v\n", solution)
+}
+
+type keyT [2]int
+type puzT struct {
+	xmin int
+	xmax int
+	ymin int
+	ymax int
+	grid map[keyT]pairT
+}
+type pairT struct {
+	sensor keyT
+	beacon keyT
+	dx     int // positive
+	dy     int // positive
+}
+
+func (p *puzT) noBeacons(row int) int {
+	return 0
+}
+
+var lineRE = regexp.MustCompile(`=(-?\d+)`)
+
+func parsePuzzle(lines []string) *puzT {
+	p := &puzT{grid: map[keyT]rune{}}
+	for _, line := range lines {
+		m := lineRE.FindAllStringSubmatch(line, -1)
+		if len(m) != 4 {
+			log.Fatalf("Unable to parse line: %v, %#v", line, m)
+		}
+		sx := must.Atoi(m[0][1])
+		sy := must.Atoi(m[1][1])
+		p.grid[keyT{sx, sy}] = 'S'
+		bx := must.Atoi(m[2][1])
+		by := must.Atoi(m[3][1])
+		p.grid[keyT{bx, by}] = 'S'
+	}
+	return p
 }
