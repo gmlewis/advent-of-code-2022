97b446fb154d250c96cdc4f81f46e42f85fea775
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-16 09:33:32 -0500

    Work on day16 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day16/part1/main.go
+++ day16/part1/main.go
@@ -6,7 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
+	"regexp"
+	"strings"
 
+	"github.com/gmlewis/advent-of-code-2021/algorithm"
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
 )
@@ -22,7 +26,94 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	puz := parsePuzzle(lines)
+	// distances := algorithm.Dijkstra[string, int](puz, "AA", nil, math.MaxInt)
+	// log.Printf("distances: %#v", distances)
+	// maxKey, maxDistance := algorithm.Max(distances, math.MaxInt)
+	// log.Printf("maxKey=%v, maxDistance=%v", maxKey, maxDistance)
+
+	solution := puz.maxPressure()
+
+	printf("Solution: %v\n", solution.pressure(30))
+}
+
+type puzT struct {
+	elapsedTime int
+	moves       []string
+	visited     map[string]bool
+	valves      map[string]*valveT
+}
+
+// puzT implements the algorithm.Graph interface.
+var _ algorithm.Graph[string, int] = &puzT{}
+
+type valveT struct {
+	openTime  int
+	flowRate  int
+	neighbors map[string]bool
+}
+
+func (p *puzT) maxPressure() *puzT {
+	return p
+}
+
+func (p *puzT) pressure(elapsedTime int) int {
+	var result int
+	for _, v := range p.valves {
+		ot := elapsedTime - v.openTime
+		if ot <= 0 {
+			continue
+		}
+		result += ot * v.flowRate
+	}
+	return result
+}
+
+var lineRE = regexp.MustCompile(`^Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.*)$`)
+
+func parsePuzzle(lines []string) *puzT {
+	p := &puzT{
+		moves:   []string{"AA"},
+		visited: map[string]bool{},
+		valves:  map[string]*valveT{},
+	}
+
+	for _, line := range lines {
+		m := lineRE.FindStringSubmatch(line)
+		if len(m) != 4 {
+			log.Fatalf("Unable to parse line: %v", line)
+		}
+		v := &valveT{
+			openTime:  30, // not open yet
+			flowRate:  must.Atoi(m[2]),
+			neighbors: map[string]bool{},
+		}
+		for _, n := range strings.Split(m[3], ", ") {
+			v.neighbors[n] = true
+		}
+		p.valves[m[1]] = v
+	}
+
+	return p
+}
+
+func (p *puzT) Distance(from, to string) int {
+	if p.valves[from].neighbors[to] {
+		return 2
+	}
+	return math.MaxInt
+}
+
+func (p *puzT) Each(f func(key string)) {
+	for k := range p.valves {
+		f(k)
+	}
+}
+
+func (p *puzT) EachNeighbor(from string, f func(from, to string)) {
+	for k := range p.valves[from].neighbors {
+		f(from, k)
+	}
 }
--- day16/part1/main_test.go
+++ day16/part1/main_test.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/test"
 )
 
+func TestPressure_Example1(t *testing.T) {
+	p := parsePuzzle(strings.Split(strings.TrimSpace(example1), "\n"))
+
+	// Enter the provided solution:
+	p.valves["DD"].openTime = 2
+	p.valves["BB"].openTime = 5
+	p.valves["JJ"].openTime = 9
+	p.valves["HH"].openTime = 17
+	p.valves["EE"].openTime = 21
+	p.valves["CC"].openTime = 24
+
+	if got, want := p.pressure(30), 1651; got != want {
+		t.Errorf("pressure = %v, want %v", got, want)
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 1651\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
