cca892343d847cbc42855cc4767a790225e0ec43
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-17 13:34:08 -0500

    Start day17 part 1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day17/part1/main.go
+++ day17/part1/main.go
@@ -26,7 +26,7 @@ func process(filename string) {
 	buf := must.ReadFile(filename)
 
 	puz := parsePuzzle(buf)
-	for i := 0; i < numRocks; i++ {
+	for i := 0; i < totalRocks; i++ {
 		puz.dropRock(i)
 		if i < 10 {
 			fmt.Printf("%v\n\n", puz)
@@ -37,21 +37,213 @@ func process(filename string) {
 }
 
 const (
-	startOffset  = 2
+	startXOffset = 2
+	startYOffset = 4
 	chamberWidth = 7
-	numRocks     = 2022
+	totalRocks   = 2022
 )
 
 type keyT [2]int
 type puzT struct {
-	time   int
-	height int
-	gas    []rune
-	grid   map[keyT]rune
+	height   int
+	gasIndex int
+	gas      []rune
+	grid     map[keyT]rune
+}
+type rockFunc func(p *puzT, startX int)
+
+var (
+	dropRockN = []rockFunc{
+		dropRock1,
+		dropRock2,
+		dropRock3,
+		dropRock4,
+		dropRock5,
+	}
+)
+
+type RenderRock func(p *puzT)
+
+func rock1(x, y int) RenderRock {
+	return func(p *puzT) {
+		p.grid[keyT{x, y}] = '#'
+		p.grid[keyT{x + 1, y}] = '#'
+		p.grid[keyT{x + 2, y}] = '#'
+		p.grid[keyT{x + 3, y}] = '#'
+	}
+}
+
+func rock2(x, y int) RenderRock {
+	return func(p *puzT) {
+		p.grid[keyT{x + 1, y}] = '#'
+		p.grid[keyT{x, y + 1}] = '#'
+		p.grid[keyT{x + 1, y + 1}] = '#'
+		p.grid[keyT{x + 2, y + 1}] = '#'
+		p.grid[keyT{x + 1, y + 2}] = '#'
+	}
+}
+
+func rock3(x, y int) RenderRock {
+	return func(p *puzT) {
+		p.grid[keyT{x, y}] = '#'
+		p.grid[keyT{x + 1, y}] = '#'
+		p.grid[keyT{x + 2, y}] = '#'
+		p.grid[keyT{x + 2, y + 1}] = '#'
+		p.grid[keyT{x + 2, y + 2}] = '#'
+	}
+}
+
+func rock4(x, y int) RenderRock {
+	return func(p *puzT) {
+		p.grid[keyT{x, y}] = '#'
+		p.grid[keyT{x, y + 1}] = '#'
+		p.grid[keyT{x, y + 2}] = '#'
+		p.grid[keyT{x, y + 3}] = '#'
+	}
+}
+
+func rock5(x, y int) RenderRock {
+	return func(p *puzT) {
+		p.grid[keyT{x, y}] = '#'
+		p.grid[keyT{x, y + 1}] = '#'
+		p.grid[keyT{x + 1, y}] = '#'
+		p.grid[keyT{x + 1, y + 1}] = '#'
+	}
+}
+
+func dropRock1(p *puzT, x int) {
+	// ####
+	if x > chamberWidth-4 {
+		x = chamberWidth - 4
+	}
+	y := p.height
+
+	noCollision := func() bool {
+		return false
+	}
+
+	for y > 0 && noCollision() {
+		y--
+	}
+
+	rock1(x, y)(p)
+
+	p.height++
+}
+
+func dropRock2(p *puzT, x int) {
+	// .#.
+	// ###
+	// .#.
+	if x > chamberWidth-3 {
+		x = chamberWidth - 3
+	}
+	y := p.height
+
+	noCollision := func() bool {
+		return false
+	}
+
+	for y > 0 && noCollision() {
+		y--
+	}
+
+	rock2(x, y)(p)
+
+	p.height += 3
+}
+
+func dropRock3(p *puzT, x int) {
+	// ..#
+	// ..#
+	// ###
+	if x > chamberWidth-3 {
+		x = chamberWidth - 3
+	}
+	y := p.height
+
+	noCollision := func() bool {
+		return false
+	}
+
+	for y > 0 && noCollision() {
+		y--
+	}
+
+	rock3(x, y)(p)
+
+	p.height += 3
+}
+
+func dropRock4(p *puzT, x int) {
+	// #
+	// #
+	// #
+	// #
+	if x > chamberWidth-1 {
+		x = chamberWidth - 1
+	}
+	y := p.height
+
+	noCollision := func() bool {
+		return false
+	}
+
+	for y > 0 && noCollision() {
+		y--
+	}
+
+	rock4(x, y)(p)
+
+	p.height += 4
+}
+
+func dropRock5(p *puzT, x int) {
+	// ##
+	// ##
+	if x > chamberWidth-2 {
+		x = chamberWidth - 2
+	}
+	y := p.height
+
+	noCollision := func() bool {
+		return false
+	}
+
+	for y > 0 && noCollision() {
+		y--
+	}
+
+	rock5(x, y)(p)
+
+	p.height += 2
 }
 
 func (p *puzT) dropRock(n int) {
-	// rockNum := n % 5
+	rockNum := n % 5
+	dxFreefall := p.getGasDx(startYOffset)
+	startX := startXOffset + dxFreefall
+	if startX < 0 {
+		startX = 0
+	}
+	dropRockN[rockNum](p, startX)
+}
+
+func (p *puzT) getGasDx(n int) int {
+	var dx int
+	for i := 0; i < n; i++ {
+		r := p.gas[p.gasIndex]
+		p.gasIndex = (p.gasIndex + 1) % len(p.gas)
+		switch r {
+		case '>':
+			dx++
+		case '<':
+			dx--
+		default:
+			log.Fatalf("Bad gas: '%c'", r)
+		}
+	}
+	return dx
 }
 
 func parsePuzzle(buf string) *puzT {
--- day17/part1/main_test.go
+++ day17/part1/main_test.go
@@ -6,10 +6,179 @@ import (
 	"github.com/gmlewis/advent-of-code-2021/test"
 )
 
+func TestDropRock(t *testing.T) {
+	tests := []struct {
+		name    string
+		puz     *puzT
+		rockNum int
+		want    *puzT
+	}{
+		{
+			name:    "initial rock1 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, rock1(0, 0)),
+		},
+		{
+			name:    "initial rock1 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, rock1(3, 0)),
+		},
+		{
+			name:    "initial rock1 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, rock1(2, 0)),
+		},
+		{
+			name:    "initial rock1 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, rock1(2, 0)),
+		},
+
+		{
+			name:    "initial rock2 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, rock2(0, 0)),
+		},
+		{
+			name:    "initial rock2 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, rock2(4, 0)),
+		},
+		{
+			name:    "initial rock2 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, rock2(2, 0)),
+		},
+		{
+			name:    "initial rock2 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, rock2(2, 0)),
+		},
+
+		{
+			name:    "initial rock3 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, rock3(0, 0)),
+		},
+		{
+			name:    "initial rock3 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, rock3(4, 0)),
+		},
+		{
+			name:    "initial rock3 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, rock3(2, 0)),
+		},
+		{
+			name:    "initial rock3 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, rock3(2, 0)),
+		},
+
+		{
+			name:    "initial rock4 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, rock4(0, 0)),
+		},
+		{
+			name:    "initial rock4 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, rock4(6, 0)),
+		},
+		{
+			name:    "initial rock4 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, rock4(2, 0)),
+		},
+		{
+			name:    "initial rock4 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, rock4(2, 0)),
+		},
+
+		{
+			name:    "initial rock5 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, rock5(0, 0)),
+		},
+		{
+			name:    "initial rock5 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, rock5(5, 0)),
+		},
+		{
+			name:    "initial rock5 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, rock5(2, 0)),
+		},
+		{
+			name:    "initial rock5 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, rock5(2, 0)),
+		},
+	}
+
+	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.puz.dropRock(tt.rockNum - 1) // 0-index
+
+			if got, want := tt.puz.height, tt.want.height; got != want {
+				t.Errorf("dropRock(%v) height = %v, want %v", tt.rockNum, got, want)
+			}
+
+			if got, want := tt.puz.gasIndex, tt.want.gasIndex; got != want {
+				t.Errorf("dropRock(%v) gasIndex = %v, want %v", tt.rockNum, got, want)
+			}
+
+			if got, want := tt.puz.String(), tt.want.String(); got != want {
+				t.Errorf("dropRock(%v) puzzle got:\n%v\nwant:\n%v", tt.rockNum, got, want)
+			}
+		})
+	}
+}
+
+func genPuz(height, gasIndex int, addRocks ...RenderRock) *puzT {
+	puz := &puzT{
+		height:   height,
+		gasIndex: gasIndex,
+		grid:     map[keyT]rune{},
+	}
+
+	for _, addRock := range addRocks {
+		addRock(puz)
+	}
+
+	return puz
+}
+
+/*
 func TestExample(t *testing.T) {
 	want := "Solution: 0\n"
 	test.Runner(t, example1, want, process, &printf)
 }
+*/
 
 func BenchmarkExample(b *testing.B) {
 	test.Benchmark(b, "../example1.txt", process, &logf, &printf)
@@ -20,6 +189,10 @@ func BenchmarkInput(b *testing.B) {
 }
 
 var example1 = `
+>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
+`
+
+/*
 ####
 
 .#.
@@ -37,9 +210,6 @@ var example1 = `
 
 ##
 ##
-
-
->>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
 
 
 The first rock begins falling:
@@ -333,4 +503,4 @@ A new rock begins falling:
 |..####.|
 +-------+
 
-`
+*/
