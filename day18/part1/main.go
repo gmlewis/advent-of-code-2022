1167657a43d7de86fd4d93ade981352d208ea5c0
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-18 07:31:02 -0500

    Solve day18 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day18/part1/main.go
+++ day18/part1/main.go
@@ -6,9 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
+	"sync"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
+	"github.com/gmlewis/advent-of-code-2021/stream"
 )
 
 var logf = log.Printf
@@ -22,7 +25,59 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	puz := &puzT{grid: map[keyT]bool{}}
+	puz = Reduce(lines, puz, parseLine)
+	surfaceArea := puz.surfaceArea()
+
+	printf("Solution: %v\n", surfaceArea)
+}
+
+type keyT [3]int
+type puzT struct {
+	grid map[keyT]bool
+}
+
+func (p *puzT) cubeSurfaceArea(ch chan<- int, key keyT) {
+	var surfaceArea int
+	f := func(k keyT) {
+		if !p.grid[k] {
+			surfaceArea++
+		}
+	}
+	f(keyT{key[0] - 1, key[1], key[2]})
+	f(keyT{key[0] + 1, key[1], key[2]})
+	f(keyT{key[0], key[1] - 1, key[2]})
+	f(keyT{key[0], key[1] + 1, key[2]})
+	f(keyT{key[0], key[1], key[2] - 1})
+	f(keyT{key[0], key[1], key[2] + 1})
+	ch <- surfaceArea
+}
+
+func (p *puzT) surfaceArea() int {
+	ch := make(chan int, 1000)
+
+	var wg sync.WaitGroup
+	wg.Add(len(p.grid))
+	for k := range p.grid {
+		go func(k keyT) {
+			p.cubeSurfaceArea(ch, k)
+			wg.Done()
+		}(k)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	return stream.Sum(ch)
+}
+
+func parseLine(line string, acc *puzT) *puzT {
+	parts := strings.Split(line, ",")
+	vs := Map(parts, must.Atoi)
+	acc.grid[keyT{vs[0], vs[1], vs[2]}] = true
+	return acc
 }
