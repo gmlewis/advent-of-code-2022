57cc7522ab42e6ac4507d04e5f9c12447af38690
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-17 21:54:38 -0500

    Start day17 part2
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day17/part2/main.go
+++ day17/part2/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -24,5 +25,156 @@ func process(filename string) {
 	logf("Processing %v ...", filename)
 	buf := must.ReadFile(filename)
 
-	printf("Solution: %v\n", len(buf))
+	puz := parsePuzzle(buf)
+	for i := 0; i < totalRocks; i++ {
+		puz.dropRock(i)
+		// if i < 10 {
+		// 	fmt.Printf("%v\n\n", puz)
+		// }
+	}
+
+	printf("Solution: %v\n", puz.height)
+}
+
+const (
+	startXOffset = 2
+	startYOffset = 4
+	chamberWidth = 7
+	totalRocks   = 1000000000000
+)
+
+type keyT [2]int64
+type puzT struct {
+	height   int64
+	gasIndex int
+	gas      []rune
+	grid     map[keyT]rune
+}
+
+// type rockFunc func(p *puzT, startX int)
+type RenderRock func(p *puzT) bool
+type rockFunc func(x, y int64, render bool) RenderRock
+type rockDef []keyT
+
+var (
+	rockFuncs = []rockFunc{rock1, rock2, rock3, rock4, rock5}
+	rock1def  = []keyT{{0, 0}, {1, 0}, {2, 0}, {3, 0}}
+	rock2def  = []keyT{{1, 0}, {0, 1}, {1, 1}, {2, 1}, {1, 2}}
+	rock3def  = []keyT{{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}}
+	rock4def  = []keyT{{0, 0}, {0, 1}, {0, 2}, {0, 3}}
+	rock5def  = []keyT{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+	rockSizes = []keyT{{4, 1}, {3, 3}, {3, 3}, {1, 4}, {2, 2}}
+)
+
+func (p *puzT) checkRock(x, y int64, rockdef rockDef, render bool) bool {
+	for _, k := range rockdef {
+		if r := p.grid[keyT{x + k[0], y + k[1]}]; r != 0 {
+			return false // collision
+		}
+	}
+	if render {
+		for _, k := range rockdef {
+			p.grid[keyT{x + k[0], y + k[1]}] = '#'
+		}
+	}
+	return true
+}
+
+func rock1(x, y int64, render bool) RenderRock {
+	return func(p *puzT) bool { return p.checkRock(x, y, rock1def, render) }
+}
+
+func rock2(x, y int64, render bool) RenderRock {
+	return func(p *puzT) bool { return p.checkRock(x, y, rock2def, render) }
+}
+
+func rock3(x, y int64, render bool) RenderRock {
+	return func(p *puzT) bool { return p.checkRock(x, y, rock3def, render) }
+}
+
+func rock4(x, y int64, render bool) RenderRock {
+	return func(p *puzT) bool { return p.checkRock(x, y, rock4def, render) }
+}
+
+func rock5(x, y int64, render bool) RenderRock {
+	return func(p *puzT) bool { return p.checkRock(x, y, rock5def, render) }
+}
+
+func (p *puzT) dropSpecificRock(rockSize keyT, fn rockFunc) {
+	x, y := int64(startXOffset), p.height+3
+	// fmt.Printf("rock starts at (%v,%v)\n", x, y)
+
+	for {
+		dx := p.getGasDx()
+		if x+dx >= 0 && x+dx+rockSize[0] <= chamberWidth && fn(x+dx, y, false)(p) {
+			x += dx
+			// dir := "right"
+			// if dx < 0 {
+			// 	dir = "left"
+			// }
+			// fmt.Printf("rock moves %v to x=%v (y=%v)\n", dir, x, y)
+		}
+
+		if y == 0 || !fn(x, y-1, false)(p) {
+			break
+		}
+		y--
+		// fmt.Printf("rock (x=%v) drops to y=%v\n", x, y)
+	}
+
+	// fmt.Printf("rock comes to rest at (%v,%v)\n", x, y)
+	if !fn(x, y, true)(p) {
+		log.Fatalf("Programming error: dropSpecificRock: fn(%v,%v,true)=false, puz:\n%v", x, y, p)
+	}
+
+	newHeight := y + rockSize[1]
+	if newHeight > p.height {
+		p.height = newHeight
+	}
+}
+
+func (p *puzT) dropRock(n int) {
+	rockNum := n % 5
+	p.dropSpecificRock(rockSizes[rockNum], rockFuncs[rockNum])
+}
+
+func (p *puzT) getGasDx() int64 {
+	r := p.gas[p.gasIndex]
+	p.gasIndex = (p.gasIndex + 1) % len(p.gas)
+	switch r {
+	case '>':
+		// fmt.Printf("gas blows right\n")
+		return 1
+	case '<':
+		// fmt.Printf("gas blows left\n")
+		return -1
+	default:
+		log.Fatalf("Bad gas: '%c'", r)
+	}
+	return 0
+}
+
+func parsePuzzle(buf string) *puzT {
+	return &puzT{
+		gas:  []rune(strings.TrimSpace(buf)),
+		grid: map[keyT]rune{},
+	}
+}
+
+func (p *puzT) String() string {
+	var lines []string
+	for y := p.height - 1; y >= 0; y-- {
+		line := "|"
+		for x := int64(0); x < chamberWidth; x++ {
+			r := p.grid[keyT{x, y}]
+			if r == 0 {
+				r = '.'
+			}
+			line += string(r)
+		}
+		line += "|"
+		lines = append(lines, line)
+	}
+	lines = append(lines, "+-------+")
+	return strings.Join(lines, "\n")
 }
--- day17/part2/main_test.go
+++ day17/part2/main_test.go
@@ -1,13 +1,243 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/test"
 )
 
+func r1(x, y int) RenderRock { return rock1(x, y, true) }
+func r2(x, y int) RenderRock { return rock2(x, y, true) }
+func r3(x, y int) RenderRock { return rock3(x, y, true) }
+func r4(x, y int) RenderRock { return rock4(x, y, true) }
+func r5(x, y int) RenderRock { return rock5(x, y, true) }
+
+func TestDropRock(t *testing.T) {
+	tests := []struct {
+		name    string
+		puz     *puzT
+		rockNum int
+		want    *puzT
+	}{
+		{
+			name:    "example 1 - rock 2",
+			puz:     genPuz(1, 4, r1(2, 0)),
+			rockNum: 2,
+			want:    genPuz(4, 8, r1(2, 0), r2(2, 1)),
+		},
+		{
+			name:    "example 1 - rock 3",
+			puz:     genPuz(4, 8, r1(2, 0), r2(2, 1)),
+			rockNum: 3,
+			want:    genPuz(6, 13, r1(2, 0), r2(2, 1), r3(0, 3)),
+		},
+		{
+			name:    "example 1 - rock 4",
+			puz:     genPuz(6, 13, r1(2, 0), r2(2, 1), r3(0, 3)),
+			rockNum: 4,
+			want:    genPuz(7, 20, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3)),
+		},
+		{
+			name:    "example 1 - rock 5",
+			puz:     genPuz(7, 20, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3)),
+			rockNum: 5,
+			want:    genPuz(9, 24, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7)),
+		},
+		{
+			name:    "example 1 - rock 6",
+			puz:     genPuz(9, 24, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7)),
+			rockNum: 6,
+			want:    genPuz(10, 28, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9)),
+		},
+		{
+			name:    "example 1 - rock 7",
+			puz:     genPuz(10, 28, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9)),
+			rockNum: 7,
+			want:    genPuz(13, 32, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10)),
+		},
+		{
+			name:    "example 1 - rock 8",
+			puz:     genPuz(13, 32, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10)),
+			rockNum: 8,
+			want:    genPuz(15, 37, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10), r3(3, 12)),
+		},
+		{
+			name:    "example 1 - rock 9",
+			puz:     genPuz(15, 37, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10), r3(3, 12)),
+			rockNum: 9,
+			want:    genPuz(17, 3, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10), r3(3, 12), r4(4, 13)),
+		},
+		{
+			name:    "example 1 - rock 10",
+			puz:     genPuz(17, 3, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10), r3(3, 12), r4(4, 13)),
+			rockNum: 10,
+			want:    genPuz(17, 12, r1(2, 0), r2(2, 1), r3(0, 3), r4(4, 3), r5(4, 7), r1(1, 9), r2(1, 10), r3(3, 12), r4(4, 13), r5(0, 12)),
+		},
+
+		{
+			name:    "initial rock1 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, r1(0, 0)),
+		},
+		{
+			name:    "initial rock1 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, r1(3, 0)),
+		},
+		{
+			name:    "initial rock1 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, r1(2, 0)),
+		},
+		{
+			name:    "initial rock1 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 1,
+			want:    genPuz(1, 4, r1(2, 0)),
+		},
+
+		{
+			name:    "initial rock2 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, r2(0, 0)),
+		},
+		{
+			name:    "initial rock2 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, r2(4, 0)),
+		},
+		{
+			name:    "initial rock2 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, r2(2, 0)),
+		},
+		{
+			name:    "initial rock2 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 2,
+			want:    genPuz(3, 4, r2(2, 0)),
+		},
+
+		{
+			name:    "initial rock3 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, r3(0, 0)),
+		},
+		{
+			name:    "initial rock3 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, r3(4, 0)),
+		},
+		{
+			name:    "initial rock3 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, r3(2, 0)),
+		},
+		{
+			name:    "initial rock3 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 3,
+			want:    genPuz(3, 4, r3(2, 0)),
+		},
+
+		{
+			name:    "initial rock4 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, r4(0, 0)),
+		},
+		{
+			name:    "initial rock4 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, r4(6, 0)),
+		},
+		{
+			name:    "initial rock4 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, r4(2, 0)),
+		},
+		{
+			name:    "initial rock4 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 4,
+			want:    genPuz(4, 4, r4(2, 0)),
+		},
+
+		{
+			name:    "initial rock5 - all left",
+			puz:     &puzT{gas: []rune("<<<<<<<<"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, r5(0, 0)),
+		},
+		{
+			name:    "initial rock5 - all right",
+			puz:     &puzT{gas: []rune(">>>>>>>>"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, r5(5, 0)),
+		},
+		{
+			name:    "initial rock5 - even mixed gas - left right",
+			puz:     &puzT{gas: []rune("><><><><"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, r5(2, 0)),
+		},
+		{
+			name:    "initial rock5 - even mixed gas - right left",
+			puz:     &puzT{gas: []rune("<><><><>"), grid: map[keyT]rune{}},
+			rockNum: 5,
+			want:    genPuz(2, 4, r5(2, 0)),
+		},
+	}
+
+	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.puz.dropRock(tt.rockNum - 1) // 0-index
+
+			if got, want := tt.puz.height, tt.want.height; got != want {
+				t.Errorf("dropRock(%v) height = %v, want %v", tt.rockNum, got, want)
+			}
+
+			if got, want := tt.puz.gasIndex, tt.want.gasIndex; got != want {
+				t.Errorf("dropRock(%v) gasIndex = %v, want %v", tt.rockNum, got, want)
+			}
+
+			if got, want := tt.puz.String(), tt.want.String(); got != want {
+				t.Errorf("dropRock(%v) puzzle got:\n%v\nwant:\n%v", tt.rockNum, got, want)
+			}
+		})
+	}
+}
+
+func genPuz(height, gasIndex int, addRocks ...RenderRock) *puzT {
+	puz := &puzT{
+		height:   height,
+		gas:      []rune(strings.TrimSpace(example1)),
+		gasIndex: gasIndex,
+		grid:     map[keyT]rune{},
+	}
+
+	for _, addRock := range addRocks {
+		addRock(puz)
+	}
+
+	return puz
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 1514285714288\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -20,317 +250,5 @@ func BenchmarkInput(b *testing.B) {
 }
 
 var example1 = `
-####
-
-.#.
-###
-.#.
-
-..#
-..#
-###
-
-#
-#
-#
-#
-
-##
-##
-
-
 >>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
-
-
-The first rock begins falling:
-|..@@@@.|
-|.......|
-|.......|
-|.......|
-+-------+
-
-Jet of gas pushes rock right:
-|...@@@@|
-|.......|
-|.......|
-|.......|
-+-------+
-
-Rock falls 1 unit:
-|...@@@@|
-|.......|
-|.......|
-+-------+
-
-Jet of gas pushes rock right, but nothing happens:
-|...@@@@|
-|.......|
-|.......|
-+-------+
-
-Rock falls 1 unit:
-|...@@@@|
-|.......|
-+-------+
-
-Jet of gas pushes rock right, but nothing happens:
-|...@@@@|
-|.......|
-+-------+
-
-Rock falls 1 unit:
-|...@@@@|
-+-------+
-
-Jet of gas pushes rock left:
-|..@@@@.|
-+-------+
-
-Rock falls 1 unit, causing it to come to rest:
-|..####.|
-+-------+
-
-A new rock begins falling:
-|...@...|
-|..@@@..|
-|...@...|
-|.......|
-|.......|
-|.......|
-|..####.|
-+-------+
-
-Jet of gas pushes rock left:
-|..@....|
-|.@@@...|
-|..@....|
-|.......|
-|.......|
-|.......|
-|..####.|
-+-------+
-
-Rock falls 1 unit:
-|..@....|
-|.@@@...|
-|..@....|
-|.......|
-|.......|
-|..####.|
-+-------+
-
-Jet of gas pushes rock right:
-|...@...|
-|..@@@..|
-|...@...|
-|.......|
-|.......|
-|..####.|
-+-------+
-
-Rock falls 1 unit:
-|...@...|
-|..@@@..|
-|...@...|
-|.......|
-|..####.|
-+-------+
-
-Jet of gas pushes rock left:
-|..@....|
-|.@@@...|
-|..@....|
-|.......|
-|..####.|
-+-------+
-
-Rock falls 1 unit:
-|..@....|
-|.@@@...|
-|..@....|
-|..####.|
-+-------+
-
-Jet of gas pushes rock right:
-|...@...|
-|..@@@..|
-|...@...|
-|..####.|
-+-------+
-
-Rock falls 1 unit, causing it to come to rest:
-|...#...|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-A new rock begins falling:
-|....@..|
-|....@..|
-|..@@@..|
-|.......|
-|.......|
-|.......|
-|...#...|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-
-|..@....|
-|..@....|
-|..@....|
-|..@....|
-|.......|
-|.......|
-|.......|
-|..#....|
-|..#....|
-|####...|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|..@@...|
-|..@@...|
-|.......|
-|.......|
-|.......|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|..@@@@.|
-|.......|
-|.......|
-|.......|
-|....##.|
-|....##.|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|...@...|
-|..@@@..|
-|...@...|
-|.......|
-|.......|
-|.......|
-|.####..|
-|....##.|
-|....##.|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|....@..|
-|....@..|
-|..@@@..|
-|.......|
-|.......|
-|.......|
-|..#....|
-|.###...|
-|..#....|
-|.####..|
-|....##.|
-|....##.|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|..@....|
-|..@....|
-|..@....|
-|..@....|
-|.......|
-|.......|
-|.......|
-|.....#.|
-|.....#.|
-|..####.|
-|.###...|
-|..#....|
-|.####..|
-|....##.|
-|....##.|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|..@@...|
-|..@@...|
-|.......|
-|.......|
-|.......|
-|....#..|
-|....#..|
-|....##.|
-|....##.|
-|..####.|
-|.###...|
-|..#....|
-|.####..|
-|....##.|
-|....##.|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
-|..@@@@.|
-|.......|
-|.......|
-|.......|
-|....#..|
-|....#..|
-|....##.|
-|##..##.|
-|######.|
-|.###...|
-|..#....|
-|.####..|
-|....##.|
-|....##.|
-|....#..|
-|..#.#..|
-|..#.#..|
-|#####..|
-|..###..|
-|...#...|
-|..####.|
-+-------+
-
 `
