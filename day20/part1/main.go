1937f278946d46b8176c126f8a118655bdbe169c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-20 17:08:18 -0500

    Start day20 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day20/part1/main.go
+++ day20/part1/main.go
@@ -22,7 +22,84 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	nums := Map(lines, must.Atoi)
+	zeroIndex, mixed := mix(nums)
+	v1 := mixed[(zeroIndex+1000)%len(mixed)]
+	v2 := mixed[(zeroIndex+2000)%len(mixed)]
+	v3 := mixed[(zeroIndex+3000)%len(mixed)]
+
+	printf("Solution: %v\n", v1+v2+v3)
+}
+
+func mix(nums []int) (int, []int) {
+	original := append([]int{}, nums...)
+
+	indices := nums2map(nums)
+
+	for _, v := range original {
+		shift(v, indices)
+	}
+
+	return map2nums(indices)
+}
+
+func shift(num int, indices map[int]int) {
+	oldIndex := indices[num]
+	var newIndex int
+	switch {
+	case num+oldIndex <= 0:
+		newIndex = (num + oldIndex - 1 + len(indices)) % len(indices)
+	case num+oldIndex >= len(indices):
+		newIndex = (num + oldIndex + 1) % len(indices)
+	default:
+		newIndex = num + oldIndex
+	}
+
+	if oldIndex == newIndex {
+		return
+	}
+	// log.Printf("shift(num=%v, indices=%v), oi=%v, ni=%v", num, indices, oldIndex, newIndex)
+	// log.Printf("moving %v from index %v to index %v", num, oldIndex, newIndex)
+
+	dx := newIndex - oldIndex
+	if dx > 0 {
+		for k, v := range indices {
+			if v > oldIndex && v <= newIndex {
+				// log.Printf("moving %v from index %v to index %v", k, v, v-1)
+				indices[k]--
+			}
+		}
+		indices[num] = newIndex
+		return
+	}
+
+	for k, v := range indices {
+		if v < oldIndex && v >= newIndex {
+			// log.Printf("moving %v from index %v to index %v", k, v, v+1)
+			indices[k]++
+		}
+	}
+	indices[num] = newIndex
+}
+
+func map2nums(indices map[int]int) (int, []int) {
+	nums := make([]int, len(indices))
+	var zeroIndex int
+	for k, v := range indices {
+		nums[v] = k
+		if k == 0 {
+			zeroIndex = v
+		}
+	}
+	return zeroIndex, nums
+}
+
+func nums2map(nums []int) map[int]int {
+	indices := map[int]int{}
+	for i, v := range nums {
+		indices[v] = i
+	}
+	return indices
 }
--- day20/part1/main_test.go
+++ day20/part1/main_test.go
@@ -4,10 +4,75 @@ import (
 	"testing"
 
 	"github.com/gmlewis/advent-of-code-2021/test"
+	"github.com/google/go-cmp/cmp"
 )
 
+func TestShift(t *testing.T) {
+	tests := []struct {
+		name    string
+		num     int
+		indices []int
+		want    []int
+	}{
+		{
+			name:    "1 moves between 2 and -3",
+			num:     1,
+			indices: []int{1, 2, -3, 3, -2, 0, 4},
+			want:    []int{2, 1, -3, 3, -2, 0, 4},
+		},
+		{
+			name:    "2 moves between -3 and 3",
+			num:     2,
+			indices: []int{2, 1, -3, 3, -2, 0, 4},
+			want:    []int{1, -3, 2, 3, -2, 0, 4},
+		},
+		{
+			name:    "-3 moves between -2 and 0",
+			num:     -3,
+			indices: []int{1, -3, 2, 3, -2, 0, 4},
+			want:    []int{1, 2, 3, -2, -3, 0, 4},
+		},
+		{
+			name:    "3 moves between 0 and 4",
+			num:     3,
+			indices: []int{1, 2, 3, -2, -3, 0, 4},
+			want:    []int{1, 2, -2, -3, 0, 3, 4},
+		},
+		{
+			name:    "-2 moves between 4 and 1",
+			num:     -2,
+			indices: []int{1, 2, -2, -3, 0, 3, 4},
+			want:    []int{1, 2, -3, 0, 3, 4, -2},
+		},
+		{
+			name:    "0 does not move",
+			num:     0,
+			indices: []int{1, 2, -3, 0, 3, 4, -2},
+			want:    []int{1, 2, -3, 0, 3, 4, -2},
+		},
+		{
+			name:    "4 moves between -3 and 0",
+			num:     4,
+			indices: []int{1, 2, -3, 0, 3, 4, -2},
+			want:    []int{1, 2, -3, 4, 0, 3, -2},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			indices := nums2map(tt.indices)
+			shift(tt.num, indices)
+			_, got := map2nums(indices)
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("shift mismatch (-want +got):\n%v", diff)
+			}
+		})
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 3\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -27,8 +92,9 @@ var example1 = `
 -2
 0
 4
+`
 
-
+/*
 Initial arrangement:
 1, 2, -3, 3, -2, 0, 4
 
@@ -52,5 +118,4 @@ Initial arrangement:
 
 4 moves between -3 and 0:
 1, 2, -3, 4, 0, 3, -2
-
-`
+*/
