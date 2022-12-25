227cf6aba1dcf0cbe17c10393653fbd3a226835d
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-12-25 08:10:02 -0500

    Solve day25 part1
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- day25/part1/main.go
+++ day25/part1/main.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
+	"strings"
 
 	. "github.com/gmlewis/advent-of-code-2021/enum"
 	"github.com/gmlewis/advent-of-code-2021/must"
@@ -22,7 +24,65 @@ func main() {
 
 func process(filename string) {
 	logf("Processing %v ...", filename)
-	buf := must.ReadFile(filename)
+	lines := must.ReadFileLines(filename)
 
-	printf("Solution: %v\n", len(buf))
+	var sum int
+	for _, line := range lines {
+		sum += snafu2dec(line)
+	}
+	solution := dec2snafu(sum)
+
+	printf("Solution: %v\n", solution)
+}
+
+func snafu2dec(s string) int {
+	var v int
+	slen := len(s)
+	for i, r := range s {
+		pow := int(math.Pow(5, float64(slen-i-1)))
+		switch r {
+		case '2':
+			v += 2 * pow
+		case '1':
+			v += pow
+		case '0':
+		case '-':
+			v -= pow
+		case '=':
+			v -= 2 * pow
+		}
+	}
+	return v
+}
+
+func dec2snafu(v int) string {
+	slen := 1
+	for {
+		if v <= snafu2dec(strings.Repeat("2", slen)) {
+			break
+		}
+		slen++
+	}
+
+	var result string
+	for slen >= 0 {
+		slen--
+		var suffix string
+		if slen > 0 {
+			suffix = strings.Repeat("=", slen)
+		}
+		switch {
+		case v >= snafu2dec(result+"2"+suffix):
+			result += "2"
+		case v >= snafu2dec(result+"1"+suffix):
+			result += "1"
+		case v >= snafu2dec(result+"0"+suffix):
+			result += "0"
+		case v >= snafu2dec(result+"-"+suffix):
+			result += "-"
+		case v >= snafu2dec(result+"="+suffix):
+			result += "="
+		}
+	}
+	return result
 }
--- day25/part1/main_test.go
+++ day25/part1/main_test.go
@@ -6,8 +6,110 @@ import (
 	"github.com/gmlewis/advent-of-code-2021/test"
 )
 
+func TestSanfu2Dec(t *testing.T) {
+	tests := []struct {
+		s    string
+		want int
+	}{
+		{s: "1", want: 1},
+		{s: "2", want: 2},
+		{s: "1=", want: 3},
+		{s: "1-", want: 4},
+		{s: "10", want: 5},
+		{s: "11", want: 6},
+		{s: "12", want: 7},
+		{s: "2=", want: 8},
+		{s: "2-", want: 9},
+		{s: "20", want: 10},
+		{s: "21", want: 11},
+		{s: "22", want: 12},
+		{s: "1==", want: 13},
+		{s: "1=-", want: 14},
+		{s: "1=0", want: 15},
+		{s: "1=1", want: 16},
+		{s: "1=2", want: 17},
+		{s: "1-=", want: 18},
+		{s: "1--", want: 19},
+		{s: "1-0", want: 20},
+		{s: "1-1", want: 21},
+		{s: "1-2", want: 22},
+		{s: "10=", want: 23},
+		{s: "10-", want: 24},
+		{s: "100", want: 25},
+		{s: "101", want: 26},
+		{s: "102", want: 27},
+		{s: "11=", want: 28},
+		{s: "11-", want: 29},
+		{s: "110", want: 30},
+		{s: "111", want: 31},
+		{s: "112", want: 32},
+		{s: "12=", want: 33},
+		{s: "12-", want: 34},
+		{s: "120", want: 35},
+		{s: "121", want: 36},
+		{s: "122", want: 37},
+		{s: "2==", want: 38},
+		{s: "2=-", want: 39},
+		{s: "2=0", want: 40},
+		{s: "2=1", want: 41},
+		{s: "2=2", want: 42},
+		{s: "2-=", want: 43},
+		{s: "2--", want: 44},
+		{s: "2-0", want: 45},
+		{s: "2-1", want: 46},
+		{s: "2-2", want: 47},
+		{s: "20=", want: 48},
+		{s: "20-", want: 49},
+		{s: "200", want: 50},
+		{s: "201", want: 51},
+		{s: "202", want: 52},
+		{s: "21=", want: 53},
+		{s: "21-", want: 54},
+		{s: "210", want: 55},
+		{s: "211", want: 56},
+		{s: "212", want: 57},
+		{s: "22=", want: 58},
+		{s: "22-", want: 59},
+		{s: "220", want: 60},
+		{s: "221", want: 61},
+		{s: "222", want: 62},
+		{s: "1===", want: 63},
+		{s: "1=11-2", want: 2022},
+		{s: "1-0---0", want: 12345},
+		{s: "1121-1110-1=0", want: 314159265},
+		{s: "1=-0-2", want: 1747},
+		{s: "12111", want: 906},
+		{s: "2=0=", want: 198},
+		{s: "21", want: 11},
+		{s: "2=01", want: 201},
+		{s: "111", want: 31},
+		{s: "20012", want: 1257},
+		{s: "112", want: 32},
+		{s: "1=-1=", want: 353},
+		{s: "1-12", want: 107},
+		{s: "12", want: 7},
+		{s: "1=", want: 3},
+		{s: "122", want: 37},
+	}
+
+	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.s, func(t *testing.T) {
+			got := snafu2dec(tt.s)
+			if got != tt.want {
+				t.Errorf("snafu2dec('%v') = %v, want %v", tt.s, got, tt.want)
+			}
+
+			back := dec2snafu(tt.want)
+			if back != tt.s {
+				t.Errorf("dec2snafu(%v) = '%v', want '%v'", tt.want, back, tt.s)
+			}
+		})
+	}
+}
+
 func TestExample(t *testing.T) {
-	want := "Solution: 0\n"
+	want := "Solution: 2=-1=0\n"
 	test.Runner(t, example1, want, process, &printf)
 }
 
@@ -33,39 +135,4 @@ var example1 = `
 12
 1=
 122
-
-
-  Decimal          SNAFU
-        1              1
-        2              2
-        3             1=
-        4             1-
-        5             10
-        6             11
-        7             12
-        8             2=
-        9             2-
-       10             20
-       15            1=0
-       20            1-0
-     2022         1=11-2
-    12345        1-0---0
-314159265  1121-1110-1=0
-
-
- SNAFU  Decimal
-1=-0-2     1747
- 12111      906
-  2=0=      198
-    21       11
-  2=01      201
-   111       31
- 20012     1257
-   112       32
- 1=-1=      353
-  1-12      107
-    12        7
-    1=        3
-   122       37
-
 `
